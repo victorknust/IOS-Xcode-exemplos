8701a36fa9f5fdf74e1edd11812cba208c34a355
Lucas Augusto <nortthon@gmail.com>
2013-03-19 20:07:41 -0300

    Revert "Revert "Primeiro projeto HelloWord""
    This reverts commit 05e88fced37b2405359b6febb2a5392d6181b55b.

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Lucas Augusto da Silva on 2013-03-08.
+//  Copyright (c) 2013 Lucas Augusto da Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HWAppDelegate class]));
+    }
+}
